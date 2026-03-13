ebd4033ccde7cd2528a1dcc1b83f142bf9a3513f
Meghana <meghanaponna234@gmail.com>
2026-03-13 09:57:46 +0530

    Update About component to correct project description from "ML Project" to "ML Projects"

--- components/About.tsx
+++ components/About.tsx
@@ -139,7 +139,7 @@ export default function About() {
 
             <div className="bg-gray-900 p-6 rounded-xl border border-gray-800 text-center">
               <h3 className="text-3xl font-bold text-blue-400">2+</h3>
-              <p className="text-gray-400 mt-2">ML Project</p>
+              <p className="text-gray-400 mt-2">ML Projects</p>
             </div>
 
           </motion.div>
